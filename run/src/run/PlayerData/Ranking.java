b7cbf799e3190a083856e01ca496cdf520c64409
wadaka <erou1227@yahoo.co.jp>
2022-06-16 15:07:11 +0900

    ランキング機能_更新

--- run/src/run/PlayerData/Ranking.java
+++ run/src/run/PlayerData/Ranking.java
@@ -5,6 +5,34 @@ public class Ranking {
 	private String score;
 	private int turn;
 	private String summry;
-	private int player_count;
+	private int play_count;
+
+	public Ranking(String name,String score,int turn,String summry,int play_count) {
+		this.name=name;
+		this.score=score;
+		this.turn=turn;
+		this.summry=summry;
+		this.play_count=play_count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public String getSummry() {
+		return summry;
+	}
+
+	public int getPlay_count() {
+		return play_count;
+	}
 
 }
